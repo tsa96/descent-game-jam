e03ea784fc26720efb19dd67a665d14b43f1b495
tsa96 <tom@trumpetdust.org>
2025-12-16 18:44:16 +0000

    Player script cleanup

--- player/player.gd
+++ player/player.gd
@@ -9,10 +9,6 @@ const TERMINAL_VELOCITY = 700.0
 const DASH_SPEED = 700.0
 
 
-## The player listens for input actions appended with this suffix.[br]
-## Used to separate controls for multiple players in splitscreen.
-@export var action_suffix: String = ""
-
 var gravity: int = ProjectSettings.get(&"physics/2d/default_gravity")
 @onready var platform_detector := $PlatformDetector as RayCast2D
 @onready var animation_player := $AnimationPlayer as AnimationPlayer
@@ -22,26 +18,27 @@ var gravity: int = ProjectSettings.get(&"physics/2d/default_gravity")
 var _double_jump_charged: bool = false
 var _dash_charged: bool = false
 
+
 func _physics_process(delta: float) -> void:
 	if is_on_floor():
 		_double_jump_charged = true
 		_dash_charged = true 
-	if Input.is_action_just_pressed("jump" + action_suffix):
+	if Input.is_action_just_pressed("jump"):
 		try_jump()
-	elif Input.is_action_just_released("jump" + action_suffix) and velocity.y < 0.0:
+	elif Input.is_action_just_released("jump") and velocity.y < 0.0:
 		# The player let go of jump early, reduce vertical momentum.
 		velocity.y *= 0.6
 	# Fall.
 	velocity.y = minf(TERMINAL_VELOCITY, velocity.y + gravity * delta)	
 
-	if Input.is_action_just_pressed("fall" + action_suffix):
+	if Input.is_action_just_pressed("fall"):
 		velocity.y = TERMINAL_VELOCITY 
 		# Player presses the fall key and falls faster after jumping
 
-	var direction := Input.get_axis("move_left" + action_suffix, "move_right" + action_suffix) * WALK_SPEED
+	var direction := Input.get_axis("move_left", "move_right") * WALK_SPEED
 	velocity.x = move_toward(velocity.x, direction, ACCELERATION_SPEED * delta)
 
-	if Input.is_action_just_pressed("dash" + action_suffix) and _dash_charged:
+	if Input.is_action_just_pressed("dash") and _dash_charged:
 		velocity.x = direction + DASH_SPEED
 		$DashTimer.start()
 
@@ -58,6 +55,7 @@ func _physics_process(delta: float) -> void:
 	if animation != animation_player.current_animation:
 		animation_player.play(animation)
 
+
 func get_new_animation() -> String:
 	var animation_new: String
 	if is_on_floor():
@@ -72,6 +70,7 @@ func get_new_animation() -> String:
 			animation_new = "jumping"
 	return animation_new
 
+
 func try_jump() -> void:
 	if is_on_floor():
 		jump_sound.pitch_scale = 1.0
@@ -83,10 +82,6 @@ func try_jump() -> void:
 		return
 	velocity.y = JUMP_VELOCITY
 	jump_sound.play()
-
-func reset() -> void:
-	position.x = 16;
-	position.y = 0;	
 
 
 func _on_dash_timer_timeout() -> void:
