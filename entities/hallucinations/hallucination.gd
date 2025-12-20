836b7a44d0220f8a178f2afdf6d0d96449084017
Brae Stoltz <braemie@protonmail.com>
2025-12-20 05:05:33 -0700

    add hallucination hit sounds

--- entities/hallucinations/hallucination.gd
+++ entities/hallucinations/hallucination.gd
@@ -1,6 +1,9 @@
 extends Area2D
 
+@onready var audio_emitter := $"../Audio/HallucinationAudioEventEmitter" as FmodEventEmitter2D
+
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
 		(body as Player).sanity_drain(10)
+		audio_emitter.play_one_shot()
 		queue_free()
