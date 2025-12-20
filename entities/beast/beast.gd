c8e46002b2d0fcf9ad836c884745d5ed0710aed6
Brae Stoltz <braemie@protonmail.com>
2025-12-20 11:24:11 -0700

    beast appear

--- entities/beast/beast.gd
+++ entities/beast/beast.gd
@@ -3,16 +3,19 @@ extends Area2D
 
 @onready var appear_audio_emitter := $Audio/MonsterAppearAudioEventEmitter as FmodEventEmitter2D
 @onready var roar_audio_emitter := $Audio/MonsterRoarAudioEventEmitter as FmodEventEmitter2D
+@onready var animator := $AnimationPlayer as AnimationPlayer
+@onready var main_sprite := $Sprite2D as Sprite2D
 
 var player_inside: Player = null
 var is_active: bool = false
 var roar_cooldown: float = 10000
 
 func _ready() -> void:
-	pass
+	main_sprite.modulate.a = 0
 
 
 func appear() -> void:
+	animator.play("appear")
 	appear_audio_emitter.play_one_shot()
 	visible = true
 	is_active = true
