9edbc6756a790afdb7d03d8c264ed53a7dcccd53
tsa96 <tom@trumpetdust.org>
2025-12-20 11:37:13 +0000

    Beast + scrolling stuff

--- entities/beast/beast.gd
+++ entities/beast/beast.gd
@@ -1,3 +1,4 @@
+class_name Beast
 extends Area2D
 
 @onready var appear_audio_emitter := $Audio/MonsterAppearAudioEventEmitter as FmodEventEmitter2D
@@ -5,29 +6,48 @@ extends Area2D
 
 var player_inside: Player = null
 var is_active: bool = false
+var roar_cooldown: float = 10000
 
 func _ready() -> void:
-	# TODO: Appear later
-	appear()
+	pass
+
 
 func appear() -> void:
 	appear_audio_emitter.play_one_shot()
+	visible = true
 	is_active = true
+
+
+func unappear() -> void:
+	visible = false
+	is_active = false
+	roar_cooldown = 10000
+
 
 func roar() -> void:
 	roar_audio_emitter.play_one_shot()
+	roar_cooldown = 0
 
-func _physics_process(_delta: float) -> void:
+
+func _physics_process(delta: float) -> void:
+	roar_cooldown += delta
 	if not is_active:
 		return
 	
-	if player_inside != null:
-		player_inside.sanity_gain(-2.0)
+	if player_inside == null:
+		return
+	
+	print(delta)
+	player_inside.sanity_gain(-2.0)
+	if roar_cooldown > 100:
+		roar()
+
 
 func _on_body_entered(body: Node2D) -> void:
 	var player = (body as Player)
 	if player != null:
 		player_inside = player
+
 
 func _on_body_exited(body: Node2D) -> void:
 	var player = (body as Player)
--- game.gd
+++ game.gd
@@ -1,9 +1,9 @@
 class_name Game
 extends Node
 
-
 @onready var world := $World
 @onready var player := $World/Player as Player
+@onready var beast := $World/Sticky/Beast as Beast
 @onready var pause_menu := $Interface/PauseMenu as PauseMenu
 @onready var death_screen := $Interface/DeathScreen as DeathScreen
 @onready var hud := $Interface/HUD as HUD
@@ -11,37 +11,54 @@ extends Node
 @onready var bgm := $Audio/BGMEventEmitter as FmodEventEmitter2D
 @onready var ui_reset_audio := $Interface/UIResetEventEmitter
 
+func _ready() -> void:
+	reset()
+
+# The most important var in the game! Controls how long gameplay is generally. Player is 2 units high btw
+const CURVE_MAX_X: float = 12000.0
+
 
 # Curve resource describe how scroll speed and music change over time.
 # Y pos is the scrolling speed, and music intensity increases occur at the 3 rightmost points.
 var game_speed_curve := load("res://game_speed_curve.tres") as Curve
 # Maximum Y position on the curve. Past this point we extrapolate.
-var curve_max_x: float = 12000.0
-var curve_max_y = game_speed_curve.sample(curve_max_x)
-var curve_grad_extrapolated = (curve_max_y - game_speed_curve.sample(curve_max_x - 1000)) / 1000
-var p1 = game_speed_curve.get_point_position(1)
-var p2 = game_speed_curve.get_point_position(2)
-var p3 = game_speed_curve.get_point_position(3)
-var posMax = 0
+const CURVE_SCROLL_MULT: float = 200.0
 
+var curve_max_y = game_speed_curve.sample(CURVE_MAX_X)
+# TODO: wrongo, probs line equation wrong below. just hardcoding!
+# var curve_gradient = (curve_max_y - game_speed_curve.sample(0.95)) / (1 - 0.95)
+var p1 = game_speed_curve.get_point_position(1)[0]
+var p2 = game_speed_curve.get_point_position(2)[0]
+var p3 = game_speed_curve.get_point_position(3)[0]
+var posMax = 0
 
 func _process(_delta: float) -> void:
 	var pos = maxf(posMax, player.position.y)
 		
-	var p = pos / curve_max_x
-	if pos <= curve_max_x:
-		player.scroll_speed = game_speed_curve.sample(p) * 100
+	var p = pos / CURVE_MAX_X
+	if pos <= CURVE_MAX_X:
+		player.scroll_speed = game_speed_curve.sample(p) * CURVE_SCROLL_MULT
 	else:
 		# y = mx + c type shit
-		player.scroll_speed = pos * curve_grad_extrapolated + curve_max_y * 100
+		player.scroll_speed = (pos - CURVE_MAX_X) * 0.05 + curve_max_y * CURVE_SCROLL_MULT
+		
+	if !beast.is_active and p > p1:
+		beast.appear()
 	
 	# Note that music transitions intensities at ~0.5 and ~1. All other values are meaningless.
-	if p > p3[0]:
-		bgm.set_parameter("Intensity", 1.0)
-	elif p > p2[0]:
-		bgm.set_parameter("Intensity", 0.5)
+	var intensity: float
+	if p < p1:
+		intensity = 0.5 - ((p1 - p) / (p1 - 0 )) * 0.5
+	elif p < p2:
+		intensity = 1   - ((p2 - p) / (p2 - p1)) * 0.5
 	else:
-		bgm.set_parameter("Intensity", 0.0)
+		intensity = 1
+	
+	bgm.set_parameter("Intensity", intensity)
+		
+	print("Scroll Speed: %f, Y Pos: %f, Y Pos / MAX: %f, BGM Intensity %f" % [
+		player.scroll_speed, pos, p, intensity
+	])
 
 
 func _unhandled_input(input_event: InputEvent) -> void:
@@ -79,15 +96,19 @@ func reset() -> void:
 	pause_menu.close()
 	hud.show()
 	ui_reset_audio.play_one_shot()
+	beast.unappear()
+	
 
 func _on_player_on_reset() -> void:
 	world.ResetWorld()
+
 
 func _on_player_on_death() -> void:
 	var tree := get_tree()
 	tree.paused = true
 	death_screen.open(player.position.y)
 	get_tree().root.set_input_as_handled()
+
 
 func _on_player_on_start_death() -> void:
 	hud.hide()
