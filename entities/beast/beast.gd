9c4296a3ef6b9df279b9e9fd5717135e6f02d54b
Brae Stoltz <braemie@protonmail.com>
2025-12-20 01:25:06 -0700

    add active state to beast

--- entities/beast/beast.gd
+++ entities/beast/beast.gd
@@ -4,14 +4,23 @@ extends Area2D
 @onready var roar_audio_emitter := $Audio/MonsterRoarAudioEventEmitter as FmodEventEmitter2D
 
 var player_inside: Player = null
+var is_active: bool = false
+
+func _ready() -> void:
+	# TODO: Appear later
+	appear()
 
 func appear() -> void:
 	appear_audio_emitter.play_one_shot()
+	is_active = true
 
 func roar() -> void:
 	roar_audio_emitter.play_one_shot()
 
 func _physics_process(_delta: float) -> void:
+	if not is_active:
+		return
+	
 	if player_inside != null:
 		player_inside.sanity_gain(-2.0)
 
